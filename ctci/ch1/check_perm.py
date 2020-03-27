dccdce5c7a547c3071294ddb05ba25618b679130
Ricky Arora <your_email_here>
2020-03-27 12:36:08 -0400

    misc added

---
+++ ctci/ch1/check_perm.py
@@ -0,0 +1,2 @@
+# we can sort it if we want to save on space.
+# O(log n) time and its O(1) space
---
+++ miscQuestions/topThreeInts.java
@@ -0,0 +1,39 @@
+
+  public class topThreeInts {
+  public static int[] threeHighest(int[] A) {
+		int highest = Integer.MIN_VALUE, secondHighest = Integer.MIN_VALUE, thirdHighest = Integer.MIN_VALUE, temp = Integer.MIN_VALUE, temp2 = Integer.MIN_VALUE;
+		
+		for(int i = 0; i < A.length; i++) {
+			if(A[i] > highest) {
+				temp = highest;
+				highest = A[i];
+				if(temp > secondHighest) {
+					thirdHighest = secondHighest;
+					secondHighest = temp;
+				}
+				if(temp > thirdHighest && temp < secondHighest) {
+					thirdHighest = temp;
+				}
+			}
+			
+			else if(A[i] < highest && secondHighest < A[i]) {
+				temp2 = secondHighest;
+				secondHighest = A[i];
+				if(temp2 > thirdHighest) {
+					thirdHighest = temp2;
+				}
+			}
+			else if(A[i] < secondHighest && A[i] > thirdHighest) {
+				thirdHighest = A[i];
+			}
+			
+		}
+		int[] topThree = {highest, secondHighest, thirdHighest};
+		
+		for(int i: topThree) {
+			System.out.println(i);
+		}
+		
+		return topThree;
+  }
+  }
