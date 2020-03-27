86b3479b5bf366ccf9f659147bff6587723e6f21
Ricky Arora <your_email_here>
2020-03-27 12:38:05 -0400

    fixing syntax

--- miscQuestions/topThreeInts.java
+++ miscQuestions/topThreeInts.java
@@ -3,7 +3,7 @@
   public static int[] threeHighest(int[] A) {
 		int highest = Integer.MIN_VALUE, secondHighest = Integer.MIN_VALUE, thirdHighest = Integer.MIN_VALUE, temp = Integer.MIN_VALUE, temp2 = Integer.MIN_VALUE;
 		
-		for(int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; i++) {
 			if(A[i] > highest) {
 				temp = highest;
 				highest = A[i];
@@ -16,10 +16,10 @@
 				}
 			}
 			
-			else if(A[i] < highest && secondHighest < A[i]) {
+			else if (A[i] < highest && secondHighest < A[i]) {
 				temp2 = secondHighest;
 				secondHighest = A[i];
-				if(temp2 > thirdHighest) {
+				if (temp2 > thirdHighest) {
 					thirdHighest = temp2;
 				}
 			}
@@ -30,7 +30,7 @@
 		}
 		int[] topThree = {highest, secondHighest, thirdHighest};
 		
-		for(int i: topThree) {
+		for (int i: topThree) {
 			System.out.println(i);
 		}
 		
